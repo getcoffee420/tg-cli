bfbc78a3f5c57855c8df8f02b5050dbc9a636dea
get-coffee <vadim.ukhlin@bk.ru>
2025-12-07 21:50:35 +0300

    rebased main and dev/basic-controller

--- main/main.cpp
+++ main/main.cpp
@@ -1,25 +1,6 @@
-#include "../src/tgClient/ITgClient.hpp"
-#include "../src/tgClient/TgClientTdlib.hpp"
-#include <iostream>
+#include "../src/tgClient/tgClient.cpp"
 
 int main() {
-    TgClientTdlib a = TgClientTdlib();
-    auto temp = a.check_status();
-
-    std::cout << "waitingPhone " << (temp == TgClientTdlib::AuthState::WaitingPhone) << std::endl;
-    std::cout << "WaitingCode " << (temp == TgClientTdlib::AuthState::WaitingCode) << std::endl;
-    std::cout << "WaitingPassword " << (temp == TgClientTdlib::AuthState::WaitingPassword) << std::endl;
-    std::cout << "WaitingParameters " << (temp == TgClientTdlib::AuthState::WaitingTdlibParameters) << std::endl;
-    std::cout << "Ready " << (temp == TgClientTdlib::AuthState::Ready) << std::endl;
-    std::cout << "LogginOut " << (temp == TgClientTdlib::AuthState::LoggingOut) << std::endl;
-    std::cout << "error " << (temp == TgClientTdlib::AuthState::Error) << std::endl;
-    enum class AuthState {
-        WaitingPhone,
-        WaitingCode,
-        WaitingPassword,
-        WaitingTdlibParameters,
-        Ready,
-        LoggingOut,
-        Error,
-    };
+  tgClient example;
+  example.loop();
 }
