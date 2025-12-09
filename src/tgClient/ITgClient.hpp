32565f0e2cedb11e7c34875a92d1c3bdb5cb40eb
get-coffee <vadim.ukhlin@bk.ru>
2025-12-09 21:03:59 +0300

    added auth, added sending messages

--- src/tgClient/ITgClient.hpp
+++ src/tgClient/ITgClient.hpp
@@ -16,6 +16,8 @@ public:
     enum class AuthState {
         WaitingPhone,
         WaitingCode,
+        WaitingPassword,
+        WaitingTdlibParameters,
         Ready,
         LoggingOut,
         Error,
@@ -39,6 +41,8 @@ public:
     virtual void enter_message_code(std::string) = 0;
 
     virtual void log_out() = 0;
+
+    virtual void send_tdlib_parameters() = 0;
 
     virtual std::vector<Chat> get_chats(int limit) = 0;
 
